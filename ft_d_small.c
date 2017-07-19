603a31415e0a1536a7e5e4374e56719ec147bb32
Denys Maznytskyi <dmaznyts@student.unit.ua>
2017-07-19 20:31:22 +0300

    started working on integer numbers

--- ft_d_small.c
+++ ft_d_small.c
@@ -6,7 +6,7 @@
 /*   By: dmaznyts <dmaznyts@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/14 21:09:23 by dmaznyts          #+#    #+#             */
-/*   Updated: 2017/07/19 02:05:04 by dmaznyts         ###   ########.fr       */
+/*   Updated: 2017/07/19 18:57:25 by dmaznyts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,31 @@
 static void	ft_wif(t_ftprintf *s, char *tmp)
 {
 	char	*tmp2;
-	int		i;
+	size_t	i;
+	size_t	numlen;
 
 	i = 0;
-	s->fw <= s->prec ? (tmp2 = (char*)malloc()) :
-		(tmp2 = (char*)malloc());
-	while (i < (s->fw - (ft_strlen(tmp) + s->prec)))
+	numlen = ft_strlen(tmp);
+	if (s->fw > numlen || s->prec > numlen)
 	{
-		s->flags[1] ? () : ();
+		s->fw <= s->prec ? (tmp2 = (char*)malloc(sizeof(char) * s->prec)) :
+			(tmp2 = (char*)malloc(sizeof(char) * s->fw));
 	}
+	else
+		tmp2 = (char*)malloc(sizeof(char) * numlen);
+	while (i < (s->fw - s->prec))
+	{
+		tmp2[i] = ' ';
+		i++;
+	}
+	while (i < (s->fw - (ft_strlen(tmp))))
+	{
+		tmp2[i] = '0';
+		i++;
+	}
+	tmp2[i] = '\0';
+	s->output = (char*)malloc(sizeof(char) * s->fw + 1);
+	s->output = ft_strcat(tmp2, tmp);
 }
 
 void		ft_d_small(t_ftprintf *s, size_t *col)
@@ -35,7 +51,7 @@ void		ft_d_small(t_ftprintf *s, size_t *col)
 		*col += ft_putstr(s->output);
 	else
 	{
-		ft_wif(s);
+		ft_wif(s, tmp);
 		*col += ft_putstr(s->output);
 	}
 }
