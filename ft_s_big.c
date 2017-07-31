8a780b970b8557e31f4a0f24c432311f36571c11
Denys Maznytskyi <dmaznyts@student.unit.ua>
2017-07-31 09:52:19 +0300

    trying to fix s_big conversion

--- ft_s_big.c
+++ ft_s_big.c
@@ -6,7 +6,7 @@
 /*   By: dmaznyts <dmaznyts@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/14 21:01:28 by dmaznyts          #+#    #+#             */
-/*   Updated: 2017/07/27 17:55:40 by dmaznyts         ###   ########.fr       */
+/*   Updated: 2017/07/29 21:35:46 by dmaznyts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 //ft_wstrlen() для обрезки по пресижену
 //ft_wchar_to_char() каждый вчар_т выводит как масив чаров/интов
-
+/*
 static size_t	ft_bitcnt(int n)
 {
 	return (ft_strlen(ft_itoa_base(n, 2)));
 }
-
+*/
 static size_t	ft_wstrlen(wchar_t *s)
 {
 	size_t	len;
@@ -32,17 +32,15 @@ static size_t	ft_wstrlen(wchar_t *s)
 
 static void		ft_write(size_t v)
 {
-	size_t	size = ft_bitcnt(v);
-
-	if (size <= 7)
+	if (v <= 127)
 	{
 		ft_write_one(v);
 	}
-	else  if (size <= 11)
+	else if (v >= 128 && v <= 2047)
 	{
 		ft_write_two(v, 49280, 0);
 	}
-	else  if (size <= 16)
+	else if (v >= 2018 && v <= 65535)
 	{
 		ft_write_three(v, 14712960, 0);
 	}
--- main.c
+++ main.c
@@ -6,16 +6,19 @@
 /*   By: dmaznyts <dmaznyts@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/02 13:56:50 by dmaznyts          #+#    #+#             */
-/*   Updated: 2017/07/27 11:53:30 by dmaznyts         ###   ########.fr       */
+/*   Updated: 2017/07/29 21:35:44 by dmaznyts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <wchar.h>
+#include <locale.h>
 
 int main()
 {
-	char *A = "this is tEEEEst!";
-	printf("|%S|\t|%S|\t|\n", L"Wide string", L"Wide");
-	ft_printf("|%S|\t|%S|\t|\n", L"Wide string", L"Wide");
+	setlocale(LC_ALL, "en_US.UTF-8");
+	wchar_t *a = L"米";
+	printf("origin	|%S|%d|%s\t|%s|\n", a,*a,ft_itoa_base(*a,2), "lol");
+	ft_printf("ft	|%S|%d|%s\t|%s|\n", a,*a,ft_itoa_base(*a,2), "lol");
 	return (0);
 }
