42efe01595fb3a4a83b391e16392c4e64ff43a7f
Denys Maznytskyi <dmaznyts@student.unit.ua>
2017-08-19 12:15:37 +0300

    pointer conversion completed

--- ft_p_small.c
+++ ft_p_small.c
@@ -6,7 +6,7 @@
 /*   By: dmaznyts <dmaznyts@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/07/14 21:06:14 by dmaznyts          #+#    #+#             */
-/*   Updated: 2017/08/17 18:03:55 by dmaznyts         ###   ########.fr       */
+/*   Updated: 2017/08/19 12:14:28 by dmaznyts         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,19 +32,20 @@ static char	*ft_ret_prec(t_ftprintf *s)
 {
 	char	*tmp;
 	char	*adr;
-	size_t	i;
 
 	adr = ft_strdup(ft_sultoa_base((unsigned long)s->arg, 16));
 	tmp = ft_strdup("0x");
-	if (!s->ip)
-		i = 0;
-	else if (s->prec && s->fw)
-		i = s->prec - ft_strlen(adr);
-	else
-		i = s->fw - (s->prec + ft_strlen(adr));
-	if ((s->fw > 0 && (s->prec > 0 || !s->ip)))
+	if (adr[0] == '0')
 	{
-		tmp = ft_strjoin(tmp, ft_add_cc(i, '0'));
+		if (!s->ip)
+			tmp = ft_strjoin(tmp, "0");
+		if (s->prec > ft_strlen(adr))
+			tmp = ft_strjoin(tmp, ft_add_cc(s->prec, '0'));
+	}
+	else
+	{
+		if (s->prec > ft_strlen(adr))
+			tmp = ft_strjoin(tmp, ft_add_cc(s->prec - ft_strlen(adr), '0'));
 		tmp = ft_strjoin(tmp, adr);
 	}
 	return (tmp);
@@ -64,8 +65,6 @@ void	ft_p_small(t_ftprintf *s, size_t *col)
 	}
 	else
 	{
-//		if ((unsigned long)s->arg > 0 && ft_strlen(prec) == 2)
-//			prec = ft_strjoin(prec, ft_sultoa_base((unsigned long)s->arg, 16));
 		if (s->fw > ft_strlen(prec))
 			tmp = ft_strjoin(ft_add_cc(s->fw - ft_strlen(prec), ' '), prec);
 		else
